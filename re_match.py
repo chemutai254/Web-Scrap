45b4610a5154e0020b96cb49704a5070c7debd73
Nancy Chemutai <88308985+chemutai254@users.noreply.github.com>
2023-05-27 01:56:41 +0300

    Regex modules and functions

--- re_match.py
+++ re_match.py
@@ -2,5 +2,13 @@ import re
 
 my_sample = "Learning Regular Expressions"
 
-n = re.search("ar", my_sample)
+#n = re.search("ar", my_sample)
+#print(n)
+#print(n.span())
+#print(n.string)
+#print(n.group())
+
+#n = re.findall("ar", my_sample)
+#n = re.split("ar", my_sample)
+n = re.sub("ar", "15", my_sample)
 print(n)
